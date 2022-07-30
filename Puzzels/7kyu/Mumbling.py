1b961da07061fd273062486750ba883ae7592f26
Florian Angerer <florian.angerer.2001@gmail.com>
2022-07-30 19:14:57 +0200

    solved mumbling

---
+++ Puzzels/7kyu/Mumbling.py
@@ -0,0 +1,17 @@
+# codewars challenge link
+# https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
+
+def accum(s):
+    solution = ""
+    for i, l in enumerate(s, start=1):
+        solution += (l * i + "-").title()
+    return solution[0:-1]
+
+
+# testing
+if __name__ == "__main__":
+    print(accum("ZpglnRxqenU") == "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+    print(accum("NyffsGeyylB") == "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+    print(accum("MjtkuBovqrU") == "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+    print(accum("EvidjUnokmM") == "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+    print(accum("HbideVbxncC") == "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
